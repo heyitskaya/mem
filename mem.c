20b9fe76b5de70740463de383eb81c7db3d35a72
heyitskaya <ni23x@mtholyoke.edu>
2018-04-03 16:44:37 -0400

    Initialized header in mem_init

--- mem.c
+++ mem.c
@@ -7,24 +7,33 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdlib.h>
+//struct Header *head;
+typedef struct Header {
+           void *startAddress;
+           int size;
+           int magic;
+           struct Header* next;
+ };
+
+typedef struct List {
+         struct Header head;
+};
+
 int main(int argc, char **argv){
+	int sizeRequested = 4096;
+	void *memory = Mem_Init(4096); //virtual address
+	printf("Mem_Init returned %p\n\n", memory);
+	struct Header head;
+	head.size=sizeRequested-sizeof(head);
 	return 0;
 }
 
-typedef struct Header {
-	int size;
-	int magic;
-};
-
-/**node will have a header and next **/
-typedef struct Node{
-	int startAddress;
-	struct Header header;
-	struct node* next;
-};
+struct Header *head;
 	
 /**Calls mmap to request sizeOfRegion bytes of memory to manage, subject to rounding up **/
 void *Mem_Init(int sizeOfRegion){
+//put first header in place
+	head->size = sizeOfRegion-sizeof(head);
 	int pageSize = getpagesize();
 	if(sizeOfRegion%pageSize!=0){ //need to round up
 		int num = sizeOfRegion/pageSize;
@@ -37,12 +46,29 @@ void *Mem_Init(int sizeOfRegion){
 		perror("mmap"); 
 		exit(1);
 	}
+	head = (struct Header *)ptr;
+	(*head).size = sizeOfRegion-sizeof(*head);
+	head->startAddress = ptr;
+
 	close(fd);
 	return ptr;
 }
 
 /**Given a number of bytes to allocate, allocate memory and returns a pointer to start of allocated memory**/
-void *Mem_Alloc(int size){
+void *Mem_Alloc(int sizeRequested){
+	struct Header *node;
+	node = head;
+	while(node!=NULL){
+	//node->size
+		if((*node).size>sizeRequested + 32){
+			(*node).size=(*node).size-(sizeRequested+32); //change size
+			(*node).startAddress = (*node).startAddress + sizeRequested + 32; //change starting address
+			return;
+		}
+		else{
+			node=node->next;
+		}
+	}
 
 }
 
@@ -51,8 +77,11 @@ int Mem_Free(void *ptr){
 	return -1;
 }
 /**For debugging**/
+/**
 void MemDump(){
-
-}
+	printf("%s\n", "Free memory");
+	printf("address = %p\n\n",memory);
+	printf("size = %d\n", sizeRequested);
+}**/
 
 
